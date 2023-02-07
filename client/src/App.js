8da9ad470cd1024bca8961301090ecffeeca87d5
Lindsay Taylor <llindsayttaylor@gmail.com>
2023-02-06 22:21:54 -0500

    installed react-router-domv6 and imported google font

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import LandingPage from './components/landing_page';
 
 function App() {
   return (
     <div className="App">
-      <h1>app</h1>
+      <LandingPage />
     </div>
   );
 }
---
+++ client/src/components/landing_page.js
@@ -0,0 +1,8 @@
+
+function LandingPage (){
+    return(
+        <h1>Lindsay Taylor</h1>
+    )
+}
+
+export default LandingPage;
