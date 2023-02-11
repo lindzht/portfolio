8f514f63875327d9ab432f9ba33fb16dcc5a89aa
Lindsay Taylor <llindsayttaylor@gmail.com>
2023-02-11 15:55:07 -0500

    installed npm package react-responsive for usemediaquery

--- client/src/components/landing_page.js
+++ client/src/components/landing_page.js
@@ -1,7 +1,18 @@
+import {useMediaQuery} from 'react-responsive'
+
+
 
 function LandingPage (){
+
+
+    const isDesktop = useMediaQuery({
+        query: '(min-width: 1224px)'
+      })
+
     return(
-        <h1>Lindsay Taylor</h1>
+        <div id="landing-page-container">
+           {isDesktop ? <h1>Lindsay Taylor</h1> : <><h1>Lindsay</h1> <br /> <h1>Taylor</h1></>}
+        </div>
     )
 }
 
