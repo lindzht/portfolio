3932e999034c4123cf852b238e7a13a8eb76785c
Lindsay Taylor <llindsayttaylor@gmail.com>
2023-02-15 17:05:54 -0500

    styled about page. set up anchor link to about section from landing for mobile

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -7,11 +7,8 @@ function About (){
     return(
         <div id="about-container">
             <h1>hi there</h1>
-            {/* <Icon name="home"></Icon> */}
-
 
             <div id="bio">
-                    {/* <h3>about me</h3> */}
                     <p>Full Stack Software Engineer with experience in Ruby on Rails and JavaScript based programming, with a background in operations. Experienced working in fast paced environments, building teams from the ground up with minimal resources. Strong cross collaborator, project leader, with experience in data analysis, who thrives in finding solutions at scale. Visual artist with a passion for creative problem solving and building beautiful, accessible apps.
                 </p>
             </div>
@@ -20,28 +17,34 @@ function About (){
 
             <div id="skills">
                 <h3>technical skills</h3>
-                <ul>
-                    <li>
+                <p>
                     Ruby on Rails, Ruby, Sinatra, React, SQLite, Javascript, Active Record, HTML, CSS, Git/GitHub, web development, wireframing, user testing
-                    </li>
-                </ul>
+                </p>
 
             </div>
             <div id="education">
                 <h3>school</h3>
-                <ul>
-                    <li>Flatiron School
+                <div className="edu">
+                    <p>
+                        <span>Flatiron School </span>
+                        <br/>
+                        Oct 2022 - Jan 2023
+                        <br />
                         Full Stack Web Development, Ruby on Rails and JavaScript program 
-                        Remote—10/2022 - 01/2023
-                    </li>
-                    <li>
-                    Carnegie Mellon University
-                    BFA in Painting, Printmaking, Drawing 
-                    University Honors
-                    Pittsburgh, PA—03/2012
-                    </li>
-                </ul>
-
+                        <br/>
+                    </p>
+                </div>
+                <div className="edu">
+                    <p>
+                        <span>Carnegie Mellon University</span>
+                        <br/>
+                        May 2012 
+                        <br />
+                        BFA in Painting, Printmaking, Drawing 
+                        <br />
+                        University Honors
+                    </p>
+                </div>
             </div>
 
 
