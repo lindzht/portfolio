ffda3df681989057c567e79e0f2f6847bf1e2569
Lindsay Taylor <llindsayttaylor@gmail.com>
2023-03-06 12:32:50 -0500

    moved background circle to landing component only

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -13,6 +13,8 @@ function LandingPage({ projects, setCurrentProject }) {
 
     return (
         <div id="landing-page">
+            <div id="bkgr-overlay"></div>
+            <div id="bkgr-circle"></div> 
 
             <NavLinks />
 
