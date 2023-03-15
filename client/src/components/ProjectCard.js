e5ebc6e397ef6966cd4218597d6615e21cbfb1d8
Lindsay Taylor <llindsayttaylor@gmail.com>
2023-03-15 14:27:21 -0400

    added lang icons for pojects on desktop

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -1,26 +1,33 @@
+import javascript from '../images/javascript.png'
+import React from '../images/react.png'
+import rails from '../images/rails.png'
 
+function ProjectCard({ name, thumbnail, date_created, skills }) {
 
-function ProjectCard ({name, thumbnail, date_created, skills}) {
-
-    function renderSkills () {
-        return( skills.map((skill) => {
-           return( <p>{skill.name}</p>)
-        })) 
+    function renderSkills() {
+        return (skills.map((skill) => {
+            if (skill.name === "React") {
+                return (<img className="p-icon" src={React} alt={skill.name} />)
+            } else if (skill.name === "JavaScript") {
+                return (<img className="p-icon" src={javascript} alt={skill.name} />)
+            } else if (skill.name === "Ruby on Rails") {
+                return (<img className="p-icon" src={rails} alt={skill.name} />)
+            }
+        }))
     }
 
 
-    return(
+    return (
         <div className="proj_card">
-           { thumbnail ? 
-                <>
-                <img src={thumbnail} alt={name} /> 
-                {/* <div className="p_details">
-                    <p>{name}</p>
-                    <p>{date_created}</p>
-                </div> */}
-                </>
+            {thumbnail ?
+               
+                <img className="img_card" src={thumbnail} alt={name} />
+                
                 
                 : <h4>{name}</h4>}
+                <div className="p_details">
+                    {renderSkills()}
+                </div>
         </div>
     )
 }
